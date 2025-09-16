b08cc44f0d69d5234a0831d5e1ae11c865318546
Ash McConnel <ashley.mcconnel@connect.qut.edu.au>
2025-09-16 13:49:41 +1000

    Updated LoginForm and RegisterForm for use in authentication templates

--- a2_group2/SeminarHub/forms.py
+++ a2_group2/SeminarHub/forms.py
@@ -1,21 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, BooleanField, TelField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, DataRequired
 
-# creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
-    submit = SubmitField("Login")
+    email = StringField('Email address', validators=[InputRequired(message='Enter your email address.'), Email(message='Enter a valid email address.')], render_kw={'class': 'auth-form-control'})
+    password = PasswordField('Password', validators=[InputRequired(message='Please enter your password.')], render_kw={'class': 'auth-form-control'})
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log In', render_kw={'class': 'mt-4 btn btn-primary login-button btn-lg'})
 
- # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
-
-    # submit button
-    submit = SubmitField("Register")
+    first_name = StringField('First name', validators=[InputRequired(message='Please enter a first name.')], render_kw={'class': 'auth-form-control'})
+    last_name = StringField('Last name', validators=[InputRequired(message='Please enter a last name.')], render_kw={'class': 'auth-form-control'})
+    email = StringField('Email address', validators=[InputRequired(message='Please enter an email address.'), Email(message='Enter a valid email address.')], render_kw={'class': 'auth-form-control'})
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, message='Must be 6 characters long or more.'), DataRequired()], render_kw={'class': 'auth-form-control'})
+    confirm_password = PasswordField('Confirm password', validators=[EqualTo('password', message='Passwords must match.')], render_kw={'class': 'auth-form-control'})
+    number = TelField('Phone number', validators=[InputRequired(message='Please enter your number.')], render_kw={'class': 'auth-form-control'})
+    address = StringField('Street address', validators=[InputRequired(message='Please enter your street address.')], render_kw={'class': 'auth-form-control'})
+    accept_toc = BooleanField('I accept all Terms and Conditions', validators=[InputRequired(message="You must accept the terms and conditions to continue.")])
+    submit = SubmitField('Create Account', render_kw={'class': 'mt-4 btn btn-primary login-button btn-lg'})
