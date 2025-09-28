533560e8caad62083c01400954a92d21feddb23a
kaido234 <tonydough212@gmail.com>
2025-09-29 09:18:49 +1000

    Added routes for navbar from home page

--- a2_group2/SeminarHub/views.py
+++ a2_group2/SeminarHub/views.py
@@ -5,3 +5,12 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@main_bp.route('/create')
+def create():
+    return render_template('create.html')
+
+@main_bp.route('/bookings')
+def booking():
+    return render_template('bookings.html')
