73f42c10761f06b4016d797041c0cbe1e1b500ca
Ash McConnel <ashley.mcconnel@connect.qut.edu.au>
2025-09-30 21:54:20 +1000

    Implemented flask and WTForm into create page

--- a2_group2/SeminarHub/forms.py
+++ a2_group2/SeminarHub/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, BooleanField, TelField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, BooleanField, TelField, SelectField, DateField, TimeField, IntegerField
+from wtforms.validators import InputRequired, Email, EqualTo, NumberRange
+from flask_wtf.file import FileRequired, FileField, FileAllowed
 # Custom validators for password and phone number checks
 from .auth import auth_validators
+
+# Allowed file types for uploaded images
+ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
 class LoginForm(FlaskForm):
     email = StringField('Email address', validators=[InputRequired(message='Enter your email address.'), Email(message='Enter a valid email address.')], render_kw={'class': 'auth-form-control'})
@@ -20,3 +24,18 @@ class RegisterForm(FlaskForm):
     address = StringField('Street address', validators=[InputRequired(message='Please enter your street address.')], render_kw={'class': 'auth-form-control'})
     accept_toc = BooleanField('I accept all Terms and Conditions', validators=[InputRequired(message="You must accept the terms and conditions to continue.")])
     submit = SubmitField('Create Account', render_kw={'class': 'mt-4 btn btn-primary login-button btn-lg'})
+
+class CreateForm(FlaskForm):
+    title = StringField('Seminar Title', validators = [InputRequired(message='Please enter a title.')])
+    category = SelectField('Category', choices=[('cs', 'Computer Science'), ('business', 'Business'), ('engineering', 'Engineering'), ('medicine', 'Medicine'), ('social', 'Social Sciences'), ('other', 'Other')])
+    description = TextAreaField('Description', validators=[InputRequired(message='Please enter a decription of your seminar.')])
+    date = DateField('Date', validators=[InputRequired(message='Please enter a valid date.')])
+    start_time = TimeField('Start Time', validators=[InputRequired(message='Please enter a valid start time.')])
+    end_time = TimeField('End Time', validators=[InputRequired(message='Please enter a valid end time.')])
+    location = StringField('Location', validators=[InputRequired(message='Please enter a location for the seminar.')])
+    capacity = IntegerField('Capacity', validators=[InputRequired(message='Please enter the maximum attendees.'), NumberRange(min=0)])
+    speaker = StringField('Speaker Name', validators=[InputRequired(message='Please enter the speakers name.')])
+    speaker_bio = TextAreaField('Speaker Bio')
+    image = FileField('Seminar Image', validators=[FileRequired(message='Please select an image to upload.'), FileAllowed(ALLOWED_FILE, message='Uploaded image must be a PNG, JPG or JPEG.')])
+    accept_toc = accept_toc = BooleanField('I confirm that I have the rights to organize this event and agree to the ', validators=[InputRequired(message="You must accept the terms and conditions to continue.")])
+    submit = SubmitField('Create Seminar', render_kw={'class': 'mt-4 btn btn-primary login-button btn-lg'})
