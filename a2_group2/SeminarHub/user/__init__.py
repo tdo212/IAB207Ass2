9e15cf2537ad3e3d0721080a0ba28d4701bf4550
Ash McConnel <ashley.mcconnel@connect.qut.edu.au>
2025-10-15 16:25:28 +1000

    Created new forms for edit profile and password templates

--- a2_group2/SeminarHub/user/__init__.py
+++ a2_group2/SeminarHub/user/__init__.py
@@ -1,5 +1,8 @@
 """
 This module is responsible for anything specific to a user profile:
 - Viewing the users hosted events
+- Viewing the users comments
 - Viewing the users profile
+- Editing the users profile
+- Viewing settings (Not implemented for this iteration)
 """
